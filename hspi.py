4970be8fadd06fe895c0962047e1d926cfb19386
Hans Baier <hansfbaier@gmail.com>
2022-06-03 04:40:39 +0700

    get rid of old CRC code

--- hspi.py
+++ hspi.py
@@ -6,63 +6,6 @@ from amaranth.build import Platform
 
 from amlib.stream import StreamInterface
 
-class HSPICRC(Elaboratable):
-    def __init__(self) -> None:
-        self.reset_in  = Signal()
-        self.enable_in = Signal()
-        self.data_in   = Signal(32)
-        self.crc_out   = Signal(32)
-
-    def elaborate(self, platform: Platform) -> Module:
-        m = Module()
-        new_crc     = Signal(32)
-        current_crc = Signal(32, reset=0xffffffff)
-
-        xor_reduce = lambda signal: reduce(operator.xor, signal)
-        bits       = lambda list, indices: [list[i] for i in indices]
-        calculate  = lambda indices: xor_reduce(Cat(bits(current_crc, indices), bits(self.data_in, indices)))
-
-        m.d.comb += [
-            new_crc[0] .eq(calculate([0, 6, 9, 10, 12, 16, 24, 25, 26, 28, 29, 30, 31])),
-            new_crc[1] .eq(calculate([0, 1, 6, 7, 9, 11, 12, 13, 16, 17, 24, 27, 28])),
-            new_crc[2] .eq(calculate([0, 1, 2, 6, 7, 8, 9, 13, 14, 16, 17, 18, 24, 26, 30, 31])),
-            new_crc[3] .eq(calculate([1, 2, 3, 7, 8, 9, 10, 14, 15, 17, 18, 19, 25, 27, 31])),
-            new_crc[4] .eq(calculate([0, 2, 3, 4, 6, 8, 11, 12, 15, 18, 19, 20, 24, 25, 29, 30, 31])),
-            new_crc[5] .eq(calculate([0, 1, 3, 4, 5, 6, 7, 10, 13, 19, 20, 21, 24, 28, 29])),
-            new_crc[6] .eq(calculate([1, 2, 4, 5, 6, 7, 8, 11, 14, 20, 21, 22, 25, 29, 30])),
-            new_crc[7] .eq(calculate([0, 2, 3, 5, 7, 8, 10, 15, 16, 21, 22, 23, 24, 25, 28, 29])),
-            new_crc[8] .eq(calculate([0, 1, 3, 4, 8, 10, 11, 12, 17, 22, 23, 28, 31])),
-            new_crc[9] .eq(calculate([1, 2, 4, 5, 9, 11, 12, 13, 18, 23, 24, 29])),
-            new_crc[10].eq(calculate([0, 2, 3, 5, 9, 13, 14, 16, 19, 26, 28, 29, 31])),
-            new_crc[11].eq(calculate([0, 1, 3, 4, 9, 12, 14, 15, 16, 17, 20, 24, 25, 26, 27, 28, 31])),
-            new_crc[12].eq(calculate([0, 1, 2, 4, 5, 6, 9, 12, 13, 15, 17, 18, 21, 24, 27, 30, 31])),
-            new_crc[13].eq(calculate([1, 2, 3, 5, 6, 7, 10, 13, 14, 16, 18, 19, 22, 25, 28, 31])),
-            new_crc[14].eq(calculate([2, 3, 4, 6, 7, 8, 11, 14, 15, 17, 19, 20, 23, 26, 29])),
-            new_crc[15].eq(calculate([3, 4, 5, 7, 8, 9, 12, 15, 16, 18, 20, 21, 24, 27, 30])),
-            new_crc[16].eq(calculate([0, 4, 5, 8, 12, 13, 17, 19, 21, 22, 24, 26, 29, 30])),
-            new_crc[17].eq(calculate([1, 5, 6, 9, 13, 14, 18, 20, 22, 23, 25, 27, 30, 31])),
-            new_crc[18].eq(calculate([2, 6, 7, 10, 14, 15, 19, 21, 23, 24, 26, 28, 31])),
-            new_crc[19].eq(calculate([3, 7, 8, 11, 15, 16, 20, 22, 24, 25, 27, 29])),
-            new_crc[20].eq(calculate([4, 8, 9, 12, 16, 17, 21, 23, 25, 26, 28, 30])),
-            new_crc[21].eq(calculate([5, 9, 10, 13, 17, 18, 22, 24, 26, 27, 29, 31])),
-            new_crc[22].eq(calculate([0, 9, 11, 12, 14, 16, 18, 19, 23, 24, 26, 27, 29, 31])),
-            new_crc[23].eq(calculate([0, 1, 6, 9, 13, 15, 16, 17, 19, 20, 26, 27, 29, 31])),
-            new_crc[24].eq(calculate([1, 2, 7, 10, 14, 16, 17, 18, 20, 21, 27, 28, 30])),
-            new_crc[25].eq(calculate([2, 3, 8, 11, 15, 17, 18, 19, 21, 22, 28, 29, 31])),
-            new_crc[26].eq(calculate([0, 3, 4, 6, 10, 18, 19, 20, 22, 23, 24, 25, 26, 28, 31])),
-            new_crc[27].eq(calculate([1, 4, 5, 7, 11, 19, 20, 21, 23, 24, 25, 26, 27, 29])),
-            new_crc[28].eq(calculate([2, 5, 6, 8, 12, 20, 21, 22, 24, 25, 26, 27, 28, 30])),
-            new_crc[29].eq(calculate([3, 6, 7, 9, 13, 21, 22, 23, 25, 26, 27, 28, 29, 31])),
-            new_crc[30].eq(calculate([4, 7, 8, 10, 14, 22, 23, 24, 26, 27, 28, 29, 30])),
-            new_crc[31].eq(calculate([5, 8, 9, 11, 15, 23, 24, 25, 27, 28, 29, 30, 31])),
-
-            self.crc_out.eq(current_crc),
-        ]
-
-        m.d.sync += current_crc.eq(Mux(self.reset_in, 0xffffffff, Mux(self.enable_in, new_crc, current_crc)))
-
-        return m
-
 class CRC(Elaboratable):
     def __init__(self, *, polynomial, crc_size, datawidth, init=None, delay=False):
         self.datawidth   = datawidth
